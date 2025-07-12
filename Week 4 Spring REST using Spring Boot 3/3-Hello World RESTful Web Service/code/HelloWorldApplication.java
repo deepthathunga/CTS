b43b1d1214d9beb951101efb42b673eb73cfe3df
deepthathunga <160524111+deepthathunga@users.noreply.github.com>
2025-07-12 10:28:00 +0530

    Add files via upload

---
+++ Week 4 Spring REST using Spring Boot 3/3-Hello World RESTful Web Service/code/HelloWorldApplication.java
@@ -0,0 +1,9 @@
+package com.vignesh.helloworld;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloWorldApplication extends Application {
+    // No implementation needed
+}
